49b1d88e36d5d92353d01fc907e4a82cf71f8dec
yxym0203 <1500081802@qq.com>
2022-06-28 10:26:46 +0800

    一个开源rpc框架项目---解决tcp拆包解码，解压缩补充

--- ms-rpc/src/main/java/com/yacoding/rpc/compress/OtherCompress.java
+++ ms-rpc/src/main/java/com/yacoding/rpc/compress/OtherCompress.java
@@ -1,6 +1,13 @@
 package com.yacoding.rpc.compress;
 
 import com.yacoding.rpc.constant.CompressTypeEnum;
+import com.yacoding.rpc.exception.MsRpcException;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 /**
  * @Author yaCoding
@@ -15,11 +22,40 @@ public class OtherCompress implements Compress{
 
     @Override
     public byte[] compress(byte[] bytes) {
-        return new byte[0];
+        //压缩
+        if (bytes == null) {
+            throw new NullPointerException("传入的压缩数据为null。。。");
+        }
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        try {
+            GZIPOutputStream gzip = new GZIPOutputStream(os);
+
+            gzip = new GZIPOutputStream(os);
+            gzip.write(bytes);
+            gzip.flush();
+            gzip.finish();
+            return os.toByteArray();
+        } catch (IOException e) {
+            throw new MsRpcException("压缩数据出错", e);
+        }
     }
 
     @Override
     public byte[] decompress(byte[] bytes) {
-        return new byte[0];
+        if (bytes == null) {
+            throw new NullPointerException("传入的解压数据为null");
+        }
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        try {
+            GZIPInputStream gzipInputStream = new GZIPInputStream(new ByteArrayInputStream(bytes));
+            byte[] buffer =  new byte[1024 * 4];
+            int n;
+            while ((n = gzipInputStream.read(buffer)) > -1){
+                os.write(buffer,0,n);
+            }
+            return os.toByteArray();
+        } catch (IOException e) {
+            throw new MsRpcException("解压缩数据出错", e);
+        }
     }
 }
